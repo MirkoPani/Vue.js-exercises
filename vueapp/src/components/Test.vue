57c01f7c9789901f9cd5cf8e442ace79900c4e18
Mirko Pani <panimirko95@gmail.com>
2017-09-12 11:38:44 +0200

    Due esercitazioni

---
+++ vueapp/src/components/Test.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="test">
+        <input type="text" v-model="title"><br/>
+        <h1>{{title}}</h1>
+        <p v-if="showName">{{user.firstName}}</p>
+        <p v-else>Nobody</p>
+        <ul>
+            <li v-for="item in items" :key="item.title">{{item.title}}</li>
+
+        </ul>
+        <button v-on:click="greet('Hello world')" >Say Greeting</button>
+
+        <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+    
+    <label>First Name:</label><input type="text" v-model="user.firstName"><br/> 
+    <label>Last Name:</label><input type="text" v-model="user.lastName"><br/> 
+    <h3>{{fullname}}</h3>
+    <h2>{{msg}}</h2>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'test',
+    props:{
+        msg:{
+            type: String,
+            default:'Foobar'
+        }
+    },
+    data() {
+        return {
+            title: 'Hello World',
+            user: {
+                firstName: 'John',
+                lastName: 'Dow'
+            },
+            showName: true,
+            items: [
+                { title: 'item1' },
+                { title: 'item2' },
+                { title: 'item3' }
+            ]
+        }
+    },
+    methods:{
+        greet:function(greeting){
+            alert(greeting);
+        },
+        pressKey:function(e){
+            console.log(e.target.value);
+        },
+        enterHit:function(){
+            console.log("You hit enter");
+        }
+    },
+    computed:{
+        fullname:function(){
+            return this.user.firstName+' '+this.user.lastName;
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ vuejsfirebase/src/App.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1> Vue.js 2 e Firebase Sample App</h1>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Add book</h3>
+      </div>
+      <div class="panel-body">
+        <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+
+          <div class="form-group">
+            <label for="bookUrl">URL:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+
+          <input type="submit" class="btn btn-primary" value="Add Book">
+
+        </form>
+      </div>
+
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3>Books List</h3>
+        </div>
+        <div class="panel-body">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>
+                  Title
+                </th>
+                <th>
+                  Author
+                </th>
+                <th>
+                  Delete
+                </th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="book in books">
+                <td>
+                  <a v-bind:href="book.url">{{book.title}}</a>
+                </td>
+                <td>
+                  {{book.author}}
+                </td>
+                <td>
+                  <span class="glyphicon glyphicon-trash" v-on:click="removeBook(book)"></span>
+                </td>
+              </tr>
+            </tbody>
+
+          </table>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+import Firebase from 'firebase'
+
+import toastr from 'toastr'
+
+let config = {
+  apiKey: "AIzaSyAtLVip3W3HBF32-rfsEK6JOdl7iQiFdAM",
+  authDomain: "vuejs-firebase-1b24a.firebaseapp.com",
+  databaseURL: "https://vuejs-firebase-1b24a.firebaseio.com",
+  projectId: "vuejs-firebase-1b24a",
+  storageBucket: "vuejs-firebase-1b24a.appspot.com",
+  messagingSenderId: "822017558664"
+}
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+
+let booksRef = db.ref("books");
+
+export default {
+  name: 'app',
+  firebase: {
+    books: booksRef
+  },
+  data() {
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
+  },
+  methods: {
+    addBook: function() {
+      booksRef.push(this.newBook);
+      this.newBook.title = "";
+      this.newBook.author = "";
+      this.newBook.url = "";
+    },
+    removeBook: function(book) {
+      booksRef.child(book['.key']).remove();
+      toastr.success("Book removed");
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
